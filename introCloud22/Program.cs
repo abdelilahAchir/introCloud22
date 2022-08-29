0831a2cafd9ca127bbcaec203b561e4e87524a0c
Abdelilah Achir <abdiachir@gmail.com>
2022-08-29 14:50:28 +0200

    ask for my name

--- introCloud22/Program.cs
+++ introCloud22/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hola, Abdelilah!");
+Console.WriteLine("whats your name");
+var name = Console.ReadLine();
+Console.WriteLine("hola," + name);
